dcc676764eb4fa29887e99926f8d5cbf7fc08adc
Julien Bouquillon <julien@bouquillon.com>
2016-02-04 14:29:02 +0100

    make it react-compliant

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,5 @@
 'use strict';
 
-var assign = require('object-assign');
 var cx = require('classnames');
 var blacklist = require('blacklist');
 var React = require('react');
@@ -9,12 +8,76 @@ module.exports = React.createClass({
   displayName: 'VideoJS',
 
   componentDidMount: function componentDidMount() {
+    // due to the DOM-destructive nature of VideosJS
+    // we cant render the video tag directly as react element
+    // so we add this part to the DOM on componentDidMount
+    // this is necessary if VideoJS needs to update its DOM later
+    var video = document.createElement('video');
+    var source = document.createElement('source');
+    video.appendChild(source);
+
+    var attrs = blacklist(this.props, 'children', 'className', 'src', 'type', 'onPlay', 'onPlayerInit', 'options');
+    attrs.class = cx(this.props.className, 'videojs', 'video-js vjs-default-skin');
+
+    Object.keys(attrs).forEach(function (key) {
+      video.setAttribute(key, attrs[key]);
+    });
+
+    if (this.props.options) {
+      video.setAttribute('data-setup', JSON.stringify(this.props.options));
+    }
+
+    source.setAttribute('src', this.props.src);
+    source.setAttribute('type', this.props.type);
+
+    this.refs.target.appendChild(video);
+
     var self = this;
-    var player = videojs(this.refs.video, this.props.options).ready(function () {
+
+    videojs(video, this.props.options).ready(function () {
       self.player = this;
       self.player.on('play', self.handlePlay);
+
+      if (self.props.onPlayerInit) {
+        self.props.onPlayerInit(this.player);
+      }
+
+      self.togglePlatformUI(self.props.type);
     });
-    if (this.props.onPlayerInit) this.props.onPlayerInit(player);
+  },
+  componentWillUnmount: function componentWillUnmount() {
+    if (this.player) {
+      this.player.dispose();
+    }
+  },
+  togglePlatformUI: function togglePlatformUI(platform) {
+    // when src changes, restore initial UI
+    // and adjust based on platform
+    this.player.controls(true);
+    if (this.player.bigPlayButton) {
+      this.player.bigPlayButton.show();
+    }
+    // disable per platform
+    if (platform === 'video/youtube') {
+      // youtube has a mandatory play button
+      if (this.player.bigPlayButton) {
+        this.player.bigPlayButton.hide();
+      }
+    }
+    if (platform === 'video/vimeo') {
+      // vimeo has mandatory controls
+      this.player.controls(false);
+    }
+  },
+  componentWillReceiveProps: function componentWillReceiveProps(nextProps) {
+    if (this.player) {
+      this.player.pause();
+      this.player.src({
+        src: nextProps.src,
+        type: nextProps.type
+      });
+      this.togglePlatformUI(nextProps.type);
+    }
   },
 
   handlePlay: function handlePlay() {
@@ -22,21 +85,6 @@ module.exports = React.createClass({
   },
 
   render: function render() {
-    var props = blacklist(this.props, 'children', 'className', 'src', 'type', 'onPlay');
-    props.className = cx(this.props.className, 'videojs', 'video-js vjs-default-skin');
-
-    assign(props, {
-      ref: 'video'
-    });
-
-    return React.createElement(
-      'div',
-      null,
-      React.createElement(
-        'video',
-        props,
-        React.createElement('source', { src: this.props.src, type: this.props.type })
-      )
-    );
+    return React.createElement('div', { ref: 'target' });
   }
 });
